781b4e56029396e3e22849ebfbc3809e28609157
Alison Wu <alison.wu23@gmail.com>
2023-04-12 11:19:43 +0800

    feat: MUI style, use ThemeProvider, CommonButton

---
+++ src/globalTheme.jsx
@@ -0,0 +1,43 @@
+import { createTheme } from '@mui/material/styles';
+
+export const globalTheme = createTheme({
+  components: {
+    // name of the component
+    MuiButton: {
+      styleOverrides: {
+        // name of the slot
+        root: {
+          fontSize: '14px',
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -6,27 +6,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Authentication from './pages/Authentication/Authentication';
 import DateTime from './pages/DateTime/DateTime';
 import Form from './pages/Form/Form';
+import { ThemeProvider } from '@mui/material/styles';
+import { globalTheme } from './globalTheme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route
-        path='/'
-        element={<App />}
-      >
+  <ThemeProvider theme={globalTheme}>
+    <BrowserRouter>
+      <Routes>
         <Route
-          path='authentication'
-          element={<Authentication />}
-        />
-        <Route
-          path='date-time'
-          element={<DateTime />}
-        />
-        <Route
-          path='form'
-          element={<Form />}
-        />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+          path='/'
+          element={<App />}
+        >
+          <Route
+            path='authentication'
+            element={<Authentication />}
+          />
+          <Route
+            path='date-time'
+            element={<DateTime />}
+          />
+          <Route
+            path='form'
+            element={<Form />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
