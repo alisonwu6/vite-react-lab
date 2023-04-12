62593fafadc5ddc463c85ac86cc077d4f4592344
Alison Wu <alison.wu23@gmail.com>
2023-04-12 15:17:23 +0800

    feat: Header and other related components

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,29 +1,26 @@
 export const navbarStyles = {
   drawer: {
-    width: 180,
+    width: 320,
     flexShrink: 0,
     '& .MuiDrawer-paper': {
-      width: 180,
+      width: 320,
       boxSizing: 'border-box',
-      backgroundColor: '#f3ce88',
-      color: '#063970',
+      backgroundColor: '#101F33',
+      color: 'rgba(255, 255, 255, 0.7)',
     },
     '& .Mui-selected': {
       color: 'red',
     },
   },
   icons: {
-    minWidth: '40px',
-    color: '#063970!important',
-    '& svg': {
-      fontSize: '20px',
-    },
+    color: 'rgba(255, 255, 255, 0.7)!important',
+    marginLeft: '20px',
   },
   text: {
     '& span': {
       marginLeft: '-10px',
       fontWeight: '600',
-      fontSize: '14px',
+      fontSize: '16px',
     },
   },
 };
--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,50 +1,12 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
-import CommonButton from '../../components/common/CommonButton/CommonButton';
 
 const Authentication = () => {
-  const buttonStyles = {
-    // fontSize: '0.875rem',
-    fontWeight: 600,
-    textTransform: 'capitalize',
-    borderRadius: 2.5,
-    '&.MuiButton-contained': {
-      color: '#fff',
-      backgroundColor: '#009be5',
-      '&:hover': {
-        backgroundColor: '#006db3',
-      },
-    },
-    '&.MuiButton-outlined': {
-      color: '#3C486B',
-      borderColor: '#3C486B',
-      '&:hover': {
-        color: '#fff',
-        backgroundColor: '#3C486B',
-      },
-    },
-  };
-
   return (
     <Grid
       item
       xs={8}
-    >
-      This is authentication page.
-      <CommonButton
-        color='secondary'
-        sx={buttonStyles}
-        variant='contained'
-      >
-        Add user
-      </CommonButton>
-      <CommonButton
-        sx={buttonStyles}
-        variant='outlined'
-      >
-        Add user
-      </CommonButton>
-    </Grid>
+    ></Grid>
   );
 };
 
